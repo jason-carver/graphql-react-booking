3f6935765c8e902a09a405389fd68972b60f80d2
“Jason”-carver <jbrittcarver@hotmail.com>
2020-06-12 21:48:53 -0500

    added cancel booking

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,7 +1,13 @@
 const {buildSchema} = require('graphql');
 
 module.exports = buildSchema(`
-
+type Booking{
+    _id:ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAt: String!
+}
 
 
 type Event{
@@ -30,12 +36,15 @@ input UserInput {
     password: String!
 }
 type RootQuery {
-    events: [Event!]
+    events: [Event!]!
+    bookings:[Booking!]!
 }
 
 type RootMutation{
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
+    bookEvent(eventId:ID!): Booking!
+    cancelBooking(bookingId: ID!): Event!
 }
     schema {
         query: RootQuery
