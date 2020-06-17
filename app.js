453106ba8bd4d63f6d1a579fe35e39a9036cc3fe
“Jason”-carver <jbrittcarver@hotmail.com>
2020-06-17 16:27:20 -0500

    added auth and refactored

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const mongoose =require('mongoose');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQLResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
 
 
 const app = express();
 
-
-
 app.use(bodyParser.json());
+app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({
   schema: graphQlSchema,
