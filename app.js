dff666ee98199c1bf415e0746dea4657e26241d3
“Jason”-carver <jbrittcarver@hotmail.com>
2020-06-20 23:51:23 -0500

    added frontend and connected to API

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+res.setHeader('Access-Control-Allow-Origin','*');
+res.setHeader('Access-Control-Allow-Methods','POST,Get,OPTIONS');
+res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+if(req.method === 'OPTIONS'){
+    return res.sendStatus(200);
+}
+next();
+});
+
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({
@@ -22,7 +34,7 @@ app.use('/graphql', graphqlHttp({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-rwdpf.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
 .then(() =>{
-    app.listen(3000);
+    app.listen(8000);
  
 })
 .catch(err =>{
