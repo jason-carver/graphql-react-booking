8b6689565edc5a34d1a195631d481946c616a69e
“Jason”-carver <jbrittcarver@hotmail.com>
2020-06-21 00:28:10 -0500

    fixing git

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const mongoose =require('mongoose');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQLResolvers = require('./graphql/resolvers/index');
-const isAuth = require('./middleware/is-auth');
 
 
 const app = express();
+
+
 
 app.use(bodyParser.json());
 
